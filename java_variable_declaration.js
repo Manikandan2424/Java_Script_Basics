d6d180158e5790a98c33e1a2b1cabdee4c42b76a
Manikandan2424 <manikandant.21ece@kongu.edu>
2024-01-22 07:20:29 +0530

    Java variable declaration

---
+++ java_variable_declaration.js
@@ -0,0 +1,9 @@
+a='a'
+console.log("1. General variable declaration . OUTPUT :",a)
+var b='var'
+console.log('2. This variable including ',b)
+let c='let'
+console.log('3. let type ',c)
+const d='100'
+console.log('4. const data type ',d)
+
