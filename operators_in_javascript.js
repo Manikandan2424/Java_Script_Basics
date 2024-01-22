fa471e0f1e56f596f716385297cc7051806f1c6c
Manikandan2424 <manikandant.21ece@kongu.edu>
2024-01-22 07:21:13 +0530

    operators_in_javascript

---
+++ operators_in_javascript.js
@@ -0,0 +1,26 @@
+// Assignment Opertors
+var a=10
+console.log("Assignement Operator output : ",a)
+
+// Arithmetic operators
+var b=10
+var c=10
+console.log("Arithmetic Operator output : ",b+c)
+
+// to check datatype
+console.log(typeof(a))
+
+console.log(100||20&&0)
+console.log(100&&0)
+console.log(100||20)
+console.log(!100)
+console.log((100||20)&&0)
+
+// using if else
+if(a==b)
+{
+    console.log("True")
+}
+else{
+    console.log("False")
+}
