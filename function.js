64995c439b6e27ef59eb5cd688ffcc18170da6a0
Manikandan2424 <manikandant.21ece@kongu.edu>
2024-01-23 09:30:31 +0530

    Function

---
+++ function.js
@@ -0,0 +1,31 @@
+// Function
+//Example 1 => Function without argument and without return type
+function func()
+{
+    console.log("Function Example")
+}
+func()
+
+//Example 2 => Function with argument and without return type
+function fun(a,b)
+{
+    console.log("Function with argument and without return type")
+    for(var i=a;i<b;i++)
+    {
+        if(i%2==0)
+    {
+        console.log("The Number ",i," is EVEN")
+    }
+    else
+    {
+        console.log("The Number ",i," is ODD")
+    }
+    } 
+}
+fun(5,10)
+
+//Example 3 => Function with argument and with return type
+
+
+
+
