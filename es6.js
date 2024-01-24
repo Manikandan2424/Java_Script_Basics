7625315e4d0d85511cccf9d9ce2decd4f9e50f13
Manikandan2424 <manikandant.21ece@kongu.edu>
2024-01-24 08:53:30 +0530

    es6 example

---
+++ es6.js
@@ -0,0 +1,14 @@
+
+
+/*
+//3.Destroying operator - D_Opertor
+var array=[10,20,30,40,50]
+var [a,b,c,d,e] =array
+console.log(a,b,c,d,e)
+
+
+//4. Spread Operator
+var array=[10,20,30,40,50]
+var arr1=[...array,60,70]
+console.log(arr1)
+*/
